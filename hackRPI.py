5b699c978c3188dbb3ad2ab60d35424afc8699c2
Arnoldas Kurbanovas <akurbanovas@albany.edu>
2016-11-12 15:52:08 -0500

    test 3 musks

--- hackRPI.py
+++ hackRPI.py
@@ -3,18 +3,18 @@
 #read in two json files and compare to see if any changes made
 #print out any changes in files
 
-sourcefolder = '/Users/arnoldas/Desktop/Fall 2016/ASRC/sourcefolder/'
-targetfolder = '/Users/arnoldas/Desktop/Fall 2016/ASRC/targetfolder/'
-targetfile = '/Users/arnoldas/Desktop/Fall 2016/ASRC/sourcefolder/20161002_reconstruction_wind_data.csv'
+sourcefolder = '/Users/arnoldas/Desktop/Fall 2016/hackRPI/sourcefolder/'
+targetfolder = '/Users/arnoldas/Desktop/Fall 2016/hackRPI/targetfolder/'
+targetfile = '/Users/arnoldas/Desktop/Fall 2016/hackRPI/sourcefolder/'
 outputfilenameprefix = 'test1'
 
 
-sample_json1=[{"filePath": 72, "permission": 0, "contentChanged": "true"},
-              {"filePath": 77, "permission": 3, "contentChanged": "false"}]
+sample_json1=[{"filePath": '/Users/arnoldas/Desktop/Fall 2016/hackRPI/sourcefolder/', "permission": 0, "contentChanged": "true"},
+              {"filePath": '/Users/arnoldas/Desktop/Fall 2016/hackRPI/sourcefolder/', "permission": 3, "contentChanged": "false"}]
 
-sample_json2=[{"filePath": 77, "permission": 3, "contentChanged": "false"},
-              {"filePath": 77, "permission": 3, "contentChanged": "false"}, # duplicity
-              {"filePath": 72, "permission": 0, "contentChanged": "true"}]
+sample_json2=[{"filePath": '/Users/arnoldas/Desktop/Fall 2016/hackRPI/sourcefolder/', "permission": 3, "contentChanged": "false"},
+              {"filePath": '/Users/arnoldas/Desktop/Fall 2016/hackRPI/sourcefolder/', "permission": 3, "contentChanged": "false"}, # duplicity
+              {"filePath": '/Users/arnoldas/Desktop/Fall 2016/hackRPI/sourcefolder/', "permission": 1, "contentChanged": "true"}]
 
 # dictionaries are unhashable, let's convert to strings for sorting
 sorted_1 = sorted([repr(x) for x in sample_json1])
